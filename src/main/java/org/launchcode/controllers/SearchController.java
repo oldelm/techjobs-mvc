9188ec770c65b99132cdbcb96416734ef8a35d47
oldelm <goldelm@gmail.com>
2018-11-29 16:23:15 -0600

    completed first 2 TODOS

--- src/main/java/org/launchcode/controllers/SearchController.java
+++ src/main/java/org/launchcode/controllers/SearchController.java
@@ -9,6 +9,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 import java.util.ArrayList;
 import java.util.HashMap;
 
+import static org.launchcode.models.JobData.findByColumnAndValue;
+import static org.launchcode.models.JobData.findByValue;
+
 /**
  * Created by LaunchCode
  */
@@ -24,4 +27,27 @@ public class SearchController {
 
     // TODO #1 - Create handler to process search request and display results
 
+    // path will be /search/results:
+    @RequestMapping(value = "results")
+    public String search(Model model, @RequestParam String searchType, @RequestParam String searchTerm) {
+        // ^add RequestParam before searchType and searchTerm because that's
+        // how we request them from the form
+
+        ArrayList<HashMap<String, String>> jobsFound;
+        // ^ create jobsFound to hold all the jobs
+
+        // two cases: search all and search more specifically:
+        if (searchType.equals("all")){
+            jobsFound = findByValue(searchTerm);
+        } else {
+            jobsFound = findByColumnAndValue(searchType, searchTerm);
+        }
+
+        // add columns and jobsFound to the model:
+        model.addAttribute("columns", ListController.columnChoices);
+        model.addAttribute("jobs", jobsFound);
+
+        return "search";
+
+    }
 }
